203003b2474d48b887f9849faa4a418a3e3a1a4e
M. Asad Ghouri <asadghouri1994@hotmail.com>
2023-03-02 19:41:38 +0500

    added tests for fizzbuzz

--- src/main_test.rs
+++ src/main_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::addition;
+    use crate::{addition, fizz_buzz};
     use crate::subtraction;
 
     #[test]
@@ -11,5 +11,25 @@ mod tests {
     #[test]
     fn test_subtraction() {
         assert_eq!(subtraction(15i32, 10i32), 5i32);
+    }
+
+    #[test]
+    fn test_fizz_buzz() {
+        assert_eq!(fizz_buzz(15), "FizzBuzz")
+    }
+
+    #[test]
+    fn test_fizz() {
+        assert_eq!(fizz_buzz(3), "Fizz")
+    }
+
+    #[test]
+    fn test_buzz() {
+        assert_eq!(fizz_buzz(5), "Buzz")
+    }
+
+    #[test]
+    fn test_number_invalid() {
+        assert_eq!(fizz_buzz(1), "number invalid")
     }
 }

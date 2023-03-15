9baaf5d9c7e45e880d331034cfe6e174e084dd64
M. Asad Ghouri <asadghouri1994@hotmail.com>
2023-03-15 14:09:35 +0500

    yooooo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod main_test;
 
-fn linear_search_with_for(arr: &[i32], target_value: i32) -> i32 {
+fn linear_search_for_i32(arr: &[i32], target_value: i32) -> i32 {
+    for (i, item) in arr.iter().enumerate() {
+        if arr[i] == target_value {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+fn linear_search_for_i16(arr: &[i16], target_value: i16) -> i16 {
     for (i, item) in arr.iter().enumerate() {
         if arr[i] == target_value {
             return arr[i];
@@ -10,10 +19,36 @@ fn linear_search_with_for(arr: &[i32], target_value: i32) -> i32 {
 }
 
 fn main() {
-    let arr: [i32; 5] = [1, 44, 32, 100, 932];
-    let target_value: i32 = 932;
+    // what i know so far in rust
+    // variable declaration
+    let variable: i32 = 100;
+    // printing (debug, normal)
+    println!("{}", variable);
+    // data types (memory types also)
+    // arrays (tuples, fixed arrays)
+    let i32_array: [i32; 5] = [10, 20, 30, 40, 60];
+    // functions
+    // for loops
+    for i in i32_array.iter().enumerate() {
+        println!("{:?}", i);
+    }
+    // if statements
+    if variable == 100 {
+        println!("hello world");
+    } else {
+        println!("goodbye world");
+    }
 
-    let found_index = linear_search_with_for(&arr, target_value);
 
-    println!("Index of the found element => {:?}", found_index);
+    let arr_i32: [i32; 5] = [1, 44, 32, 100, 932];
+    let target_value_i32: i32 = 932;
+
+    let arr_i16: [i16; 5] = [1, 44, 32, 100, 932];
+    let target_value_i16: i16 = 100;
+
+    let found_index_i32: i32 = linear_search_for_i32(&arr_i32, target_value_i32);
+    let found_index_i16: i16 = linear_search_for_i16(&arr_i16, target_value_i16);
+
+    println!("Value of the found element => {:?}", found_index_i32);
+    println!("Value of the found element => {:?}", found_index_i16);
 }
--- src/main_test.rs
+++ src/main_test.rs
@@ -1,12 +1,36 @@
 #[cfg(test)]
 mod tests {
-    use crate::{linear_search_with_for};
+    use crate::{linear_search_for_i16, linear_search_for_i32};
 
     #[test]
-    fn linear_search() {
+    fn linear_search_i32_when_element_is_found() {
         let arr: [i32; 5] = [1, 100, 90, 23, 233];
         let target: i32 = 90;
 
-        assert_eq!(linear_search_with_for(&arr, target), 90);
+        assert_eq!(linear_search_for_i32(&arr, target), 90);
+    }
+
+    #[test]
+    fn linear_search_i32_when_element_not_found() {
+        let arr: [i32; 5] = [1, 100, 90, 23, 233];
+        let target: i32 = 101;
+
+        assert_eq!(linear_search_for_i32(&arr, target), -1);
+    }
+
+    #[test]
+    fn linear_search_i16_when_element_is_found() {
+        let arr: [i16; 5] = [1, 100, 90, 23, 233];
+        let target: i16 = 90;
+
+        assert_eq!(linear_search_for_i16(&arr, target), 90);
+    }
+
+    #[test]
+    fn linear_search_i16_when_element_not_found() {
+        let arr: [i16; 5] = [1, 100, 90, 23, 233];
+        let target: i16 = 101;
+
+        assert_eq!(linear_search_for_i16(&arr, target), -1);
     }
 }
